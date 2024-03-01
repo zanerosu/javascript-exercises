5ce447e03d466424ce971ce822965af2af1ce699
Zane Richards <zanerosu@gmail.com>
2024-02-29 20:32:03 -0500

    Completed tempConversion exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureF) {
+    temperatureC = (temperatureF - 32) * (5/9)
+    return Math.round(temperatureC * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureC) {
+    temperatureF = (temperatureC * (9/5) + 32)
+    return Math.round(temperatureF * 10) / 10
 };
 
 // Do not edit below this line
