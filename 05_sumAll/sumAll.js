6480731607b94ff938235051a99952c2094cb907
Zane Richards <zanerosu@gmail.com>
2024-02-29 20:06:07 -0500

    Completed SumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 >= 0 && num2 >= 0 && typeof(num1) == "number" && typeof(num2) == "number"){
+        let result = 0;
+        for(let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++){
+            result += i
+        }
+        return result
+    }else{
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
