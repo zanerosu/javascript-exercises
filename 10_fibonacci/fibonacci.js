683c9fc271928c68c895e83d2443f6ec9e964d65
Zane Richards <zanerosu@gmail.com>
2024-03-06 18:54:03 -0500

    Completed Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(value) {
+    value = parseInt(value)
+    if (value > 0){
+        let n1 = 0;
+        let n2 = 1;
+        let nextTerm = 1;
+        for(let i = 0; i < value - 1; i++){
+            nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
+        }
+        return nextTerm
+    }
+    else if (value === 0){
+        return 0;
+    }
+    else{
+        return "OOPS"
+    }
 };
 
 // Do not edit below this line
