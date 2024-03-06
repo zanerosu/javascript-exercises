f59f5115ec4ebe386bff24e95d9e060149482d50
Zane Richards <zanerosu@gmail.com>
2024-03-06 16:57:48 -0500

    Completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+    return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+    return nums.reduce((total, currentNum) => total + currentNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+    return nums.reduce((total, currentNum) => total * currentNum);
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+    return Math.pow(base, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+    if (num1 < 0){
+      return -1;
+    }
+    else if (num1 === 0){
+      return 1;
+    }
+    else{
+      return (num1 * factorial(num1-1));
+    }
 };
 
 // Do not edit below this line
